e43de82188cb6e8dab480b5c88b19fa8a18aeebb
Da-Chuckwalla <62317727+Da-Chuckwalla@users.noreply.github.com>
2022-01-25 23:24:00 -0500

    making the tiles, vertices, and edges clickable

--- classes.py
+++ classes.py
@@ -3,18 +3,18 @@ import pygame
 import random
 
 class Tile:
-    def __init__(self, color, x, y, number):
+    def __init__(self, color, x, y, number, points):
         self.color = color
         self.x = x
         self.y = y
         self.number = number
+        self.points = points
 
-    def draw(self, surface, color, tiltAngle, x, y, radius):
+    def draw(self, surface, color):
         pts = []
-        for i in range(6):
-            x = x + radius * math.cos(tiltAngle + math.pi * 2 * i / 6)
-            y = y + radius * math.sin(tiltAngle + math.pi * 2 * i / 6)
-            pts.append([int(x), int(y)])
+        for i in self.points:
+            pts.append(i)
+
         pygame.draw.polygon(surface, color, pts)
 
     def getThePoints(self, surface, color, tiltAngle, x, y, radius):
@@ -24,11 +24,51 @@ class Tile:
             y = y + radius * math.sin(tiltAngle + math.pi * 2 * i / 6)
             pts.append([int(x), int(y)])
         return pts
+    def getCenter(self, sidelength):
+        pointX = self.points[1][0]
+        pointY = self.points[0][1] + int(sidelength/2)
+        return [pointX, pointY]
+
 
 class Vertice:
-    def __init__(self, x, y):
+    def __init__(self, x, y, width, height):
         self.x = x
         self.y = y
+        self.width = width
+        self.height = height
+
+    def draw(self, surface, color):
+        pygame.draw.rect(surface, color, (self.x - (self.width / 2), self.y - (self.height / 2), self.width, self.height))
+
+    def pressed(self, mouse, click):
+        if (self.x - (self.width / 2) <= mouse[0] and mouse[0] <= self.x - (self.width / 2) + self.width) and (self.y - (self.height / 2) <= mouse[1] and mouse[1] <= self.y - (self.height / 2) + self.height) and click[0] == True:
+            return True
+
+
+class Edge:
+    def __init__(self, x1, y1, x2, y2):
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+    def pressed(self, mouse, click):
+        if self.x1 <= self.x2:
+            x1 = self.x1
+            x2 = self.x2
+        else:
+            x1 = self.x2
+            x2 = self.x1
+        if self.y1 <= self.y2:
+            y1 = self.y1
+            y2 = self.y2
+        else:
+            y1 = self.y2
+            y2 = self.y1
+        if (x1-3 <= mouse[0] and mouse[0] <= x2+3 ) and (y1 <= mouse[1] and mouse[1] <= y2) and click[0] == True:
+            return True
+
+
+
 
 
 
--- testing.py
+++ testing.py
@@ -1,6 +1,9 @@
 import math
-import pygame
 import random
+
+import pygame
+
+from classes import Edge
 from classes import Tile
 from classes import Vertice
 
@@ -15,6 +18,7 @@ red = (156, 67, 0)
 grey = (123, 111, 131)
 tan = (243, 192, 114)
 white = (249, 238, 225)
+black = (0, 0, 0)
 
 
 def getPoints(tiltAngle, x, y, radius):
@@ -69,105 +73,122 @@ def generateNumbers(tiles):
     return tiles
 
 
-def makeMap(x, y, sideLength):
+def makeTiles(x, y, sideLength):
     tiles = []
     tile = [x, y]
-    vertices = []
-    edges = []
+    tilePoints = []
     num = 0
     for i in range(5):
         tiles.append(tile)
         points = getPoints(11, tile[0], tile[1], sideLength)
-        for point in points:
-            if point not in vertices:
-                vertices.append(point)
-        if num >= 2:
-            if [points[5], points[0]] not in edges:
-                edges.append([points[5], points[0]])
-            for j in range(5):
-                if [points[j], points[j+1]] not in edges:
-                    edges.append([points[j], points[j+1]])
-
+        tilePoints.append(points)
         tile = points[3]
         num += 1
 
     points = getPoints(11, tiles[0][0], tiles[0][1], sideLength)
     tile = [points[4][0], points[4][1] + sideLength]
     del tiles[0:2]
-    del vertices[0:8]
-    num = 0
+    del tilePoints[0:2]
+
     for i in range(5):
         tiles.append(tile)
         points = getPoints(11, tile[0], tile[1], sideLength)
-        for point in points:
-            if point not in vertices:
-                vertices.append(point)
-        if num >= 1:
-            if [points[5], points[0]] not in edges:
-                edges.append([points[5], points[0]])
-            for j in range(5):
-                if [points[j], points[j+1]] not in edges:
-                    edges.append([points[j], points[j+1]])
-
+        tilePoints.append(points)
         tile = points[3]
         num += 1
+
     points = getPoints(11, tiles[3][0], tiles[3][1], sideLength)
     tile = [points[4][0], points[4][1] + sideLength]
     tiles.remove(tiles[3])
-    del vertices[17:24]
+    tilePoints.remove(tilePoints[3])
+
     for i in range(5):
         tiles.append(tile)
         points = getPoints(11, tile[0], tile[1], sideLength)
-        for point in points:
-            if point not in vertices:
-                vertices.append(point)
+        tilePoints.append(points)
         tile = points[3]
 
     points = getPoints(11, tiles[7][0], tiles[7][1], sideLength)
     tile = [points[4][0], points[4][1] + sideLength]
+
     for i in range(4):
         tiles.append(tile)
         points = getPoints(11, tile[0], tile[1], sideLength)
-        for point in points:
-            if point not in vertices:
-                vertices.append(point)
+        tilePoints.append(points)
         tile = points[3]
 
     points = getPoints(11, tiles[12][0], tiles[12][1], sideLength)
     tile = [points[4][0], points[4][1] + sideLength]
     for i in range(3):
         tiles.append(tile)
-
         points = getPoints(11, tile[0], tile[1], sideLength)
-        for point in points:
-            if point not in vertices:
-                vertices.append(point)
+        tilePoints.append(points)
         tile = points[3]
 
     colors = randomColors(4, 4, 3, 4, 3, 1)
     tilesList = []
-    verticeList = []
+
     number = 0
     for tile in tiles:
-        tilesList.append(Tile(colors[number], tile[0], tile[1], 1))
+        tilesList.append(Tile(colors[number], tile[0], tile[1], 1, tilePoints[number]))
         number += 1
     tilesList = generateNumbers(tilesList)
-    print(edges)
-    for i in vertices:
-        verticeList.append(Vertice(i[0], i[1]))
-    return tilesList, verticeList, edges
+
+    return tilesList
+
+def makeVertices(tiles, width, height):
+    points = []
+    for tile in tiles:
+        for point in tile.points:
+            if point not in points:
+                points.append(point)
+    for pnt in points:
+        for tile in tiles:
+            for point in tile.points:
+                if point != pnt and (point[0] >= pnt[0] - 5 and point[0] <= pnt[0] + 5) and (point[1] >= pnt[1] - 5 and point[1] <= pnt[1] + 5):
+                    tile.points[tile.points.index(point)] = pnt
+    vertices = []
+    for tile in tiles:
+        for point in tile.points:
+            if point not in vertices:
+                vertices.append(point)
+    verticeList = []
+    for vertice in vertices:
+        verticeList.append(Vertice(vertice[0], vertice[1], width, height))
+    return verticeList
+
+def makeEdges(tiles):
+    edges = []
+    for tile in tiles:
+        if [tile.points[5], tile.points[0]] and [tile.points[0], tile.points[5]] not in edges:
+            edges.append([tile.points[5], tile.points[0]])
+        num = 0
+        for i in range(5):
+            if [tile.points[i], tile.points[i+1]] and [tile.points[i+1], tile.points[i]] not in edges:
+                edges.append([tile.points[i], tile.points[i+1]])
+    edgeList = []
+    for edge in edges:
+        edgeList.append(Edge(edge[0][0], edge[0][1], edge[1][0], edge[1][1]))
+    return edgeList
+
 
 def drawTiles(tiles, sideLength):
     for i in tiles:
-        i.draw(root, i.color, 11, i.x, i.y, sideLength)
+        i.draw(root, i.color)
 
 def drawVertices(vertices):
     for i in vertices:
-        pygame.draw.rect(root, white, (i.x-5, i.y-5, 10, 10))
+        i.draw(root, white)
 
 def drawEdges(edges):
     for edge in edges:
-        pygame.draw.polygon(root, white, ((edge[0][0] - 3, edge[0][1]),  (edge[1][0] - 3, edge[1][1]), (edge[1][0] + 3, edge[1][1]), (edge[0][0] + 3, edge[0][1])))
+            if edge.x1 - 1 == edge.x2:
+                edge.x1 = edge.x2
+    for edge in edges:
+        if edge.x1 == edge.x2:
+            pygame.draw.polygon(root, white, ((edge.x1 - 2, edge.y1),  (edge.x2 - 2, edge.y2), (edge.x2 + 2, edge.y2), (edge.x1 + 2, edge.y1)))
+        else:
+            pygame.draw.polygon(root, white, ((edge.x1 - 3, edge.y1),  (edge.x2 - 3, edge.y2), (edge.x2 + 3, edge.y2), (edge.x1 + 3, edge.y1)))
 
 def drawNumbers(tiles, sidelength):
     diagonal = math.sqrt(3) * sidelength
@@ -178,6 +199,35 @@ def drawNumbers(tiles, sidelength):
         num = str(tile.number)
         textsurface = myfont.render(num, False, (0, 0, 0))
         root.blit(textsurface, (tile.x + (diagonal/2 - 5), tile.y - sidelength + 10))
+
+def verticesPressed(vertices, mouse, click):
+    for i in vertices:
+        clicked = i.pressed(mouse, click)
+        if clicked:
+            print("yes")
+
+def edgesPressed(edges, mouse, click):
+    for i in edges:
+        clicked = i.pressed(mouse, click)
+        if clicked:
+            print("yes")
+
+def tilePressed(tiles, mouse, click):
+    centers = []
+    for i in tiles:
+        centers.append(i.getCenter(60))
+    if click[0] == True:
+        shortestDistance = 1000
+        for center in centers:
+            distanceX = abs(center[0] - mouse[0])
+            distanceY = abs(center[1] - mouse[1])
+            distance = math.sqrt(distanceX**2 + distanceY**2)
+            if distance < shortestDistance:
+                shortestDistance = distance
+                tile = tiles[centers.index(center)]
+        print(tile.color)
+
+
 
 
 
@@ -193,12 +243,19 @@ while run:
 
     sideLength = 60
     if first == True:
-        tiles, vertices, edges = makeMap(50, 100, sideLength)
+        tiles = makeTiles(60, 100, sideLength)
+        vertices = makeVertices(tiles, 10, 10)
+        edges = makeEdges(tiles)
         first = False
+
     drawTiles(tiles, sideLength)
     drawNumbers(tiles, sideLength)
-    drawVertices(vertices)
+    #drawVertices(vertices)
     drawEdges(edges)
 
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
 
+    #edgesPressed(edges, mouse, click)
+    tilePressed(tiles, mouse, click)
     pygame.display.flip()
